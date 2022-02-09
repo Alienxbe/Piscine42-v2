73402d0b7d834d130fb407db6a1309467b599cf6
Martin Ykman <maykman@shi-r3-p1.s19.be>
2022-02-09 09:42:35 +0100

    Correcting norm on C01

--- c01/ex08/ft_sort_int_tab.c
+++ c01/ex08/ft_sort_int_tab.c
@@ -6,11 +6,9 @@
 /*   By: maykman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 08:44:47 by maykman           #+#    #+#             */
-/*   Updated: 2022/02/09 09:15:57 by maykman          ###   ########.fr       */
+/*   Updated: 2022/02/09 09:23:27 by maykman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
 
 void	ft_swap(int *a, int *b)
 {
@@ -21,12 +19,12 @@ void	ft_swap(int *a, int *b)
 	*b = tmp;
 }
 
-int		ft_imax(int *tab, int size)
+int	ft_imax(int *tab, int size)
 {
 	int	max;
 	int	imax;
 	int	i;
-	
+
 	max = 0;
 	i = -1;
 	while (++i < size)
