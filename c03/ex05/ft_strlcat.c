ad4705fea9b7ec120595a498b195c5859df0b3fd
Martin Ykman <maykman@shi-r2-p1.s19.be>
2022-02-15 13:22:10 +0100

    C03 : finished project (Norminated)

--- c03/ex05/ft_strlcat.c
+++ c03/ex05/ft_strlcat.c
@@ -3,14 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maykman <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: maykman <maykman@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 21:22:59 by maykman           #+#    #+#             */
-/*   Updated: 2022/02/12 21:25:46 by maykman          ###   ########.fr       */
+/*   Updated: 2022/02/15 13:21:45 by maykman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-unsigned int ft_strlcat(char *dest, char *src, unsigned int size)
+unsigned int	ft_strlen(char *str)
 {
-	
+	unsigned int	i;
+
+	i = 0;
+	while (str && str[i])
+		i++;
+	return (i);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	dest_len;
+	unsigned int	i;
+
+	dest_len = ft_strlen(dest);
+	i = -1;
+	if (dest_len <= size)
+		return (dest_len + ft_strlen(src));
+	while (src[++i] && i < size - dest_len - 1)
+		dest[dest_len + i] = src[i];
+	dest[dest_len + i] = 0;
+	return (dest_len + ft_strlen(src));
 }
